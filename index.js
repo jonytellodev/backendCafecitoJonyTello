fcd64c2b0764804e190a096d9d6a003d30793dc0
jonytellodev <jonathantello.dev@gmail.com>
2023-09-04 20:02:10 -0300

    agregamos la configuracion al DB

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import express from 'express';
+import './src/databases/database.js';
+
 console.log('esto es una prueba')
 
-//
+// 1 - configuraciones iniciales
+const app = express();
+// crear variable con express
+app.set('port', process.env.PORT || 4000);
+// 2 - middlewares
+// 3 - creacion de rutas
