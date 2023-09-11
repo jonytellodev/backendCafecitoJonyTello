266d9087945eaeb910a39d40e046cd0f7ef5ff47
jonytellodev <jonathantello.dev@gmail.com>
2023-09-10 22:24:53 -0300

    se agrega conexion a bd

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,10 +1,20 @@
-import Producto from "../models/producto"
+import Producto from "../models/producto.js";
 
 export const listarProductos = (req, res) =>{
     try {
+        res.send('esto es una pruebita')
         //ir a la bd y pedir los productos
     } catch (error) {
         console.log(error)        
     }
-    res.send('esto es una pruebita')
+}
+
+export const crearProducto = (req, res) =>{
+    try {
+        console.log(req)
+        console.log(req.body)
+        res.send('producto creado')
+    } catch (error) {
+        console.log(error)        
+    }
 }
